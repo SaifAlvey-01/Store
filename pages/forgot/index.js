e01796c5a93cafbf84913f508b52ff4f8923d186
Muhammad Usman <muhammadusmanmuzzamil@gmail.com>
2023-08-10 16:21:15 +0200

    name changed

--- pages/forgot/index.js
+++ pages/forgot/index.js
@@ -1,27 +1,9 @@
 import React, { useState } from 'react';
 import Forgot1 from '../../components/forgot1';
 
-const SignupComponent = () => {
-    //   const [currentStep, setCurrentStep] = useState(1);
-
-    //   const renderStep = () => {
-    //     switch (currentStep) {
-    //       case 1:
-    //         return <SignUp1  setCurrentStep={setCurrentStep}/>;
-    //       case 2:
-    //         return <SignUp2  setCurrentStep={setCurrentStep}/>;
-    //       case 3:
-    //         return <SignUp3 setCurrentStep={setCurrentStep}/>;
-    //       case 4:
-    //         return <SignUp5 setCurrentStep={setCurrentStep}/>;
-    //       default:
-    //         return null;
-    //     }
-    //   };
+const ForgotComponent = () => {
 
     return (
-
-
 
         <div className="bg-[url('/BackgroundMobile.png')] md:bg-[url('/Background.png')] bg-cover bg-no-repeat bg-center h-screen">
             <div className='w-full flex justify-center items-center md:justify-start md:ml-32 h-20'>
@@ -40,4 +22,4 @@ const SignupComponent = () => {
     );
 };
 
-export default SignupComponent;
+export default ForgotComponent;
