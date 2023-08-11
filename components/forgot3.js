41208b2b6b78e9dad04cf599d3319c66345ce6ae
Muhammad Usman <muhammadusmanmuzzamil@gmail.com>
2023-08-11 18:38:42 +0200

    forgot screens done

--- components/forgot3.js
+++ components/forgot3.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import Lottie from 'lottie-react'
+import animationData from '../public/animation_ll6mnwur.json'
 
 const forgot3 = () => {
   return (
-    <div>forgot3</div>
+    <div>
+      <div>
+        <div className=' bg-transparent'>
+          <Lottie
+           animationData={animationData}
+           loop={true}
+           autoplay={true}
+          />
+        </div>
+        <div className='flex flex-row justify-center items-center'>
+          Resetting Password..
+        </div>
+      </div>
+    </div>
   )
 }
 
