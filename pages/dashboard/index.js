d1580ae9fc34117ba71d4adb70d7df3f0c67cbfc
areebkhan11 <areebkhan123123@gmail.com>
2023-08-25 23:16:59 +0500

    refactor

---
+++ pages/dashboard/index.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import Sidebar from '../../components/partials/Sidebar';
+import Header from '../../components/partials/Header';
+
+
+const index = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+    return (
+        <div className="flex h-screen overflow-hidden">
+        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+          <main>
+            <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto  bg-[url('/BackgroundMobile.png')] md:bg-[url('/Background.png')] bg-cover bg-no-repeat bg-center h-screen bg-[#F6F7FF]">
+                <p>Hello</p>    
+            </div>
+          </main>
+        </div>
+      </div>
+    );
+}
+
+export default index;
