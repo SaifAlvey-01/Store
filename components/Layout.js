229b35558ffa7d022749f2b1b6ef411175d76fbb
sakhawatsyed4@gmail.com <sakhawatsyed4@gmail.com>
2023-09-01 20:42:52 +0500

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,24 +1,26 @@
-import React, { useState } from 'react';
-import Sidebar from './partials/Sidebar';
-import Header from './partials/Header';
+import React, { useState } from "react";
+import Sidebar from "./partials/Sidebar";
+import Header from "./partials/Header";
 
-
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
-    return (
-        <div className="flex h-screen overflow-hidden ">
-        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-        <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-          <main>
-            <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto h-screen ">
-               {children}  
-            </div>
-          </main>
-        </div>
+  return (
+    <div className="flex h-screen overflow-hidden ">
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+      <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <main>
+          <div
+            style={{ backgroundColor: "#F7F9FB" }}
+            className="py-6 px-2 lg:px-6 max-w-9xl mx-auto  "
+          >
+            {children}
+          </div>
+        </main>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Layout;
