228354b4bf370aebfa54f05f5a31895416f0a3a3
sakhawatsyed4@gmail.com <sakhawatsyed4@gmail.com>
2023-09-04 18:49:49 +0500

    Update step1.js

--- pages/signup/step1.js
+++ pages/signup/step1.js
@@ -59,12 +59,16 @@ const SignUp1 = ({ setCurrentStep, setFormData, setEmail }) => {
                       </div>
                       <input
                         type="text"
+                        placeholder="Enter your email "
                         {...register("email", {
-                          required: "Email is required",
-                          pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                          required: "Enter A Valid Email Address !",
+                          pattern: {
+                            value:
+                              /^(?:[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}|^\+[0-9]{10,15}$)/i,
+                            message: "Invalid email ",
+                          },
                         })}
                         onChange={(e) => {}}
-                        placeholder="Enter your email"
                         className={`focus:border-[#b3c0ff] focus:outline-none focus:ring-1 border-slate-300  self-stretch rounded-lg bg-white flex flex-row py-3.5 px-4 items-center justify-start text-[#4B4B4B] font-roboto border-[1.5px] border-solid md:border-gainsboro ${
                           errors.email ? "border-red-500" : ""
                         }`}
