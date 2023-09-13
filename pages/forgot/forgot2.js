d0366850ee9c154f81c462eaee87140e2817e9f3
areebkhan11 <areebkhan123123@gmail.com>
2023-09-14 04:22:46 +0500

    api configuration

--- pages/forgot/forgot2.js
+++ pages/forgot/forgot2.js
@@ -1,6 +1,8 @@
 import { useCallback, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Verify from "../../components/Verify/Verify";
+import useAxios from '../../hooks/useAxios';
+
 
 const Forgot2 = ({ setCurrentStep, inputValue }) => {
   const [OTP, setOTP] = useState("");
@@ -8,23 +10,26 @@ const Forgot2 = ({ setCurrentStep, inputValue }) => {
 
   const router = useRouter();
   const [active, setactive] = useState(false);
-  const handleInputChange = (index, value) => {
-    setInputValues((prevValues) => {
-      const newValues = [...prevValues];
-      newValues[index] = value;
-      return newValues;
-    });
-  };
+  const { resdata, error, loading, putData: putRequest } = useAxios();
 
-  const onTextFieldContainer1Click = useCallback(() => {
-    console.log(active);
 
-    setCurrentStep((prevStep) => prevStep + 1);
-  }, []);
+  const onTextFieldContainer1Click = () => {
+
+    putRequest('/user/verify-otp-reset-password', {userOtp:OTP , email:inputValue});
+
+    
+  }
 
   const handleSignInClick = () => {
     router.push("/login");
   };
+
+  useEffect(()=>{
+    if(resdata.message === "You Can Change Your Password Now!"){
+
+      setCurrentStep((prevStep) => prevStep + 1);
+    }
+  },[resdata])
 
   return (
     <div className=" w-full sm:w-[400px] md:w-[400px] lg:w-[450px]  text-11xl text-neutral-600">
@@ -44,6 +49,9 @@ const Forgot2 = ({ setCurrentStep, inputValue }) => {
                 <div className="w-full flex flex-row items-center justify-start  gap-[4px] cursor-pointer text-sm text-dimgray">
                   <Verify OTP={OTP} setOTP={setOTP} />
                 </div>
+                <p className="text-[#F64C4C] text-[13px] my-1 mx-1">
+                          {resdata.message}
+                          </p>
               </div>
 
               <button
@@ -56,7 +64,15 @@ const Forgot2 = ({ setCurrentStep, inputValue }) => {
                 } w-full flex flex-col p-2 box-border items-center mb-2 justify-center text-center text-white font-poppins`}
               >
                 <div className="relative w-[90px] h-0" />
-                Continue
+                { loading ? <svg aria-hidden="true" role="status" class="inline mr-3 w-6 h-6 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"></path>
+                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"></path>
+                            </svg> :
+                            <div>
+                               Continue
+                            </div>
+                            }
+                
               </button>
             </div>
             <div className="relative w-full text-sm tracking-[0.25px] leading-[20px] text-center text-darkslategray font-roboto mt-6">
--- pages/login/Login2.js
+++ pages/login/Login2.js
@@ -6,7 +6,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { simpleHash, users } from "../../utils/userData";
 import { useRouter } from "next/router";
 import Loading from "../../components/loading";
-import cogoToast from "cogo-toast";
+import useAxios from '../../hooks/useAxios';
+
 
 const Login2 = ({ inputData }) => {
   const router = useRouter();
@@ -14,6 +15,8 @@ const Login2 = ({ inputData }) => {
   const user = useSelector((state) => state.userSlice.items);
   const [passwordError, setPasswordError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const { resdata, error, loading, postData: postRequest } = useAxios();
+
 
   const {
     handleSubmit,
@@ -21,9 +24,29 @@ const Login2 = ({ inputData }) => {
     formState: { errors },
   } = useForm();
 
+  useEffect(()=>{
+    if(resdata.message === "User not existed, Try SignUp"){
+      setPasswordError("Invalid Email or password");
+    }
+    if(resdata.message === "Loged In"){
+      setPasswordError(null);
+      setIsLoading(true);
+      setTimeout(() => {
+        router.push("/dashboard");
+      }, 2000);
+
+    }
+    if(resdata.message === "Incorrect Password"){
+      setPasswordError("Invalid Email or password");
+    }
+  },[resdata])
+
   const onSubmit = (data, e) => {
     e.preventDefault();
-    const matchedUser = users.find((u) => u.email === data.email);
+    // const matchedUser = users.find((u) => u.email === data.email);
+
+    postRequest('/user/login', {email:data.email, password: data.password });
+
 
     // const data = await response.json();
     // const token = data.token; // Assuming your backend sends a token
@@ -34,24 +57,24 @@ const Login2 = ({ inputData }) => {
     // router.push('/dashboard');
 
     // No user found with the given email
-    if (!matchedUser) {
-      setPasswordError("Invalid Email or password");
-      return;
-    }
+    // if (!matchedUser) {
+     
+    //   return;
+    // }
 
     // Check password hash
-    if (matchedUser.passwordHash === simpleHash(data.password)) {
-      setPasswordError(null);
+    // if (matchedUser.passwordHash === simpleHash()) {
+    //   setPasswordError(null);
 
-      setIsLoading(true);
-      setTimeout(() => {
-        router.push("/dashboard");
-      }, 2000);
+    //   setIsLoading(true);
+    //   setTimeout(() => {
+    //     router.push("/dashboard");
+    //   }, 2000);
 
-      // cogoToast.success("User Successfully logged in");
-    } else {
-      setPasswordError("Invalid Email or password");
-    }
+    //   // cogoToast.success("User Successfully logged in");
+    // } else {
+    //   setPasswordError("Invalid Email or password");
+    // }
   };
 
   return (
@@ -125,7 +148,15 @@ const Login2 = ({ inputData }) => {
                         >
                           <div className="relative w-[90px] h-0" />
                           <div className="relative tracking-[0.02em] leading-[24px] font-medium">
-                            Log in
+                          { loading ? <svg aria-hidden="true" role="status" class="inline mr-3 w-6 h-6 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"></path>
+                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"></path>
+                            </svg> :
+                            <div>
+                               Log in
+                            </div>
+                            }
+                           
                           </div>
                         </button>
                       </div>
