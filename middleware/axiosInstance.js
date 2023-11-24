2823e96c4f0e9416c0f7d16604fb439e342f0822
Hadi2808 <hadialikhan2808@gmail.com>
2023-11-24 15:02:25 +0500

    hadi work

--- middleware/axiosInstance.js
+++ middleware/axiosInstance.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import Cookies from "js-cookie";
-import { signOut } from "next-auth/react"
-
+import { signOut } from "next-auth/react";
 
 const axiosInstance = axios.create({
   baseURL: process.env.BASE_URL,
@@ -9,28 +8,40 @@ const axiosInstance = axios.create({
 });
 
 axiosInstance.interceptors.request.use(
-  (config) => {
+  async (config) => {
     // Add an "Authorization" header if the token is available
     const token = Cookies.get("token");
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
     }
+
     if (config.url.includes("/media")) {
       config.headers["Content-Type"] = "multipart/form-data";
-    }else{
+    } else {
       config.headers["Content-Type"] = "application/json";
-      
     }
-    
+
     return config;
   },
-  (error) => {
-    localStorage.removeItem('signupCurrentStep');
-    Cookies.remove('token');
-    Cookies.remove('id');
-    signOut();
-    return Promise.reject(error);
+  async (error) => {
+    if (axios.isAxiosError(error) && error.response?.status === 401) {
+      // Token expired or invalid, perform logout
+      await handleLogout();
+    } else {
+      // Other error, reject the promise
+      return Promise.reject(error);
+    }
   }
 );
+
+const handleLogout = async () => {
+  // You can perform additional cleanup/logic before signing out
+  localStorage.removeItem('signupCurrentStep');
+  Cookies.remove('token');
+  Cookies.remove('id');
+  
+  // Sign out the user
+  await signOut();
+};
 
 export default axiosInstance;
